b637d515b906dc4e4c612adc77a6a119f59db232
adarshchougule143 <adarshchougule143@gmail.com>
2026-08-19 18:54:27 +0530

    first pattern

---
+++ Pattern1.java
@@ -0,0 +1,22 @@
+class Pattern1 {
+    public void pattern1 (int n) {
+        for( int i=0 ; i<5 ; i++ ) {
+            for (int j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+public static void main(String[] args) {
+   Pattern1 sol=new Pattern1();
+   int N = 5;
+   sol.pattern1 (N);  
+} 
+
+  }
+    
+
+
+
+    
+
