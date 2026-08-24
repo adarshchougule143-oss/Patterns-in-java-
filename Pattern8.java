9a7d9b367be5931d60d5d3125440a71fa13c6ce9
adarshchougule143 <adarshchougule143@gmail.com>
2026-08-24 19:57:04 +0530

    Pattern8

---
+++ Pattern8.java
@@ -0,0 +1,38 @@
+class Pattern8 {
+
+    
+    public void pattern8(int N) {
+
+        
+        for (int i = 0; i < N; i++) {
+
+            
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            
+            for (int j = 0; j < 2 * N - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            
+
+
+
+            System.out.println();
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        Pattern8 sol = new Pattern8();
+
+        int N = 5;
+
+        sol.pattern8(N);
+    }
+}
